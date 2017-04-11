7b002bb49aef11df59b4cb16bc84f2b2fae4e331
Rafael Rossi <rossirafael@yahoo.com>
2017-04-11 14:16:01 +0200

    mais Catenary

--- java/Catenary.java
+++ java/Catenary.java
@@ -1,18 +1,28 @@
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.LayoutManager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentAdapter;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.BoxLayout;
 import javax.swing.SwingConstants;
-import java.awt.Color;
-import java.awt.Graphics;
+import javax.swing.BorderFactory;
+
 
 public class Catenary {
 	private JFrame frame;
 	private JPanel background;
 	private DrawPanel drawPanel;
+	private final int TEXT_FIELD_COLUMNS = 5;
 	
 	public static void main(String[] args) {
 		new Catenary().start();
@@ -21,61 +31,130 @@ public class Catenary {
 		frame = new JFrame("Catenary");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel textBoxes = new JPanel();
-		textBoxes.setLayout(new BoxLayout(textBoxes, BoxLayout.Y_AXIS));
-		
-		JPanel hPanel = new JPanel();
+		GridLayout grid = new GridLayout(6, 2);
+		grid.setVgap(1);
+		grid.setHgap(2);
+		JPanel textBoxes = new JPanel(grid);
+		textBoxes.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+
 		JLabel hLabel = new JLabel(String.format("%30s", "Horizontal distance H [m]:"), SwingConstants.LEFT);
-		JTextField hText = new JTextField(10);
-		hPanel.add(hLabel);
-		hPanel.add(hText);
-		textBoxes.add(hPanel);
-		
-		JPanel vPanel = new JPanel();
+		JTextField hText = new JTextField(TEXT_FIELD_COLUMNS);
+		textBoxes.add(hLabel);
+		textBoxes.add(hText);
+
 		JLabel vLabel = new JLabel(String.format("%30s", "Vertical distance V [m]:"), SwingConstants.LEFT);
-		JTextField vText = new JTextField(10);
-		vPanel.add(vLabel);
-		vPanel.add(vText);
-		textBoxes.add(vPanel);
-		
-		JPanel tPanel = new JPanel();
+		JTextField vText = new JTextField(TEXT_FIELD_COLUMNS);
+		textBoxes.add(vLabel);
+		textBoxes.add(vText);
+
 		JLabel tLabel = new JLabel(String.format("%30s", "Top angle TA [deg]:"), SwingConstants.LEFT);
-		JTextField tText = new JTextField(10);
-		tPanel.add(tLabel);
-		tPanel.add(tText);
-		textBoxes.add(tPanel);
-		
-		JPanel lPanel = new JPanel();
+		JTextField tText = new JTextField(TEXT_FIELD_COLUMNS);
+		textBoxes.add(tLabel);
+		textBoxes.add(tText);
+
 		JLabel lLabel = new JLabel(String.format("%30s", "Length [m]:"), SwingConstants.LEFT);
-		JTextField lText = new JTextField(10);
-		lPanel.add(lLabel);
-		lPanel.add(lText);
-		textBoxes.add(lPanel);
+		JTextField lText = new JTextField(TEXT_FIELD_COLUMNS);
+		textBoxes.add(lLabel);
+		textBoxes.add(lText);
 		
+		JLabel mLabel = new JLabel(String.format("%30s", "MBR [m]:"), SwingConstants.LEFT);
+		JTextField mText = new JTextField(TEXT_FIELD_COLUMNS);
+		textBoxes.add(mLabel);
+		textBoxes.add(mText);
+		
+		JButton btnCalc = new JButton("Calculate");
+		JButton btnClear = new JButton("Clear");
+		btnCalc.addActionListener(new CalculateListener());
+		btnClear.addActionListener(new ClearListener());
+		textBoxes.add(btnCalc);
+		textBoxes.add(btnClear);
+		
+		HolderPanel drawPanelHolder = new HolderPanel(new BorderLayout());
+		drawPanelHolder.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		drawPanel = new DrawPanel();
+		drawPanelHolder.add(drawPanel, BorderLayout.CENTER);
 		
 		background = new JPanel();
 		background.setLayout(new BorderLayout());
 		background.add(BorderLayout.WEST, textBoxes);
-		background.add(BorderLayout.CENTER, drawPanel);
+		background.add(BorderLayout.CENTER, drawPanelHolder);
 		frame.getContentPane().add(background);
-		frame.setSize(300, 200);
+		frame.setLocation(200, 200);
+		frame.setSize(500, 200);
 		frame.setVisible(true);
 	}
-}
-
-class DrawPanel extends JPanel {
-	public void paintComponent(Graphics gfx) {
-		gfx.fillRect(0, 0, this.getWidth(), this.getHeight());
-		int r = (int) (Math.random()*255);
-		int g = (int) (Math.random()*255);
-		int b = (int) (Math.random()*255);
-		int x = (int) (Math.random()*(this.getWidth()-100));
-		int y = (int) (Math.random()*(this.getHeight()-100));
-		gfx.setColor(new Color(r, g, b));
-		gfx.fillOval(x, y, 100, 100);
-		int[] ptsX = {10, 150, 75, 25};
-		int[] ptsY = {10, 25, 120, 80};
-		gfx.fillPolygon(ptsX, ptsY, 4);
+	
+	/**
+	* Listeners for the Calculate and Clear buttons
+	*/
+	class CalculateListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			//calculate();
+			drawPanel.setIsDrawingReady(true);
+			drawPanel.calcPoints();
+			drawPanel.repaint();
+		}
+	}
+	class ClearListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			//clear();
+			drawPanel.setIsDrawingReady(false);
+			drawPanel.repaint();
+		}
+	}
+	/**
+	* Draws the catenary shape.
+	*/
+	class DrawPanel extends JPanel {
+		private final int NUM_PTS = 50;
+		private int[] ptsX = new int[NUM_PTS];
+		private int[] ptsY = new int[NUM_PTS];
+		private boolean isDrawingReady;
+		
+		public DrawPanel() {
+			super();
+			this.addComponentListener(new DrawPanelResizeListener());
+			isDrawingReady = false;
+		}
+		public void setIsDrawingReady(boolean b) {
+			isDrawingReady = b;
+		}
+		void calcPoints() {
+			System.out.println("recalculating pixels");
+			float stepX = (float) this.getWidth() / NUM_PTS;
+			float stepY = (float) this.getHeight() / NUM_PTS;
+			for (int i=0; i < NUM_PTS; i++) {
+				ptsX[i] = (int) stepX*i;
+				ptsY[i] = (int) stepY*i;
+			}
+		}
+		public void paintComponent(Graphics gfx) {
+			System.out.println("redrawing canvas");
+			int w = this.getWidth();
+			int h = this.getHeight();
+			gfx.setColor(Color.black);
+			gfx.fillRect(0, 0, w, h);
+			gfx.setColor(Color.white);
+			if (isDrawingReady) gfx.drawPolyline(ptsX, ptsY, NUM_PTS);
+		}
+		// listener for resize events
+		class DrawPanelResizeListener extends ComponentAdapter {
+			public void componentResized(ComponentEvent e) {
+				System.out.println("resizing window");
+				if (isDrawingReady) calcPoints();
+			}
+		}
+		
+	}
+	class HolderPanel extends JPanel {
+		public HolderPanel(LayoutManager layout) {
+			super(layout);
+		}
+		public void paintComponent(Graphics gfx) {
+			int w = this.getWidth();
+			int h = this.getHeight();
+			gfx.setColor(Color.black);
+			gfx.fillRect(0, 0, w, h);
+		}
 	}
 }
