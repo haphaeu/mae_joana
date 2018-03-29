4e2b63f5784e77e4c8cfa0b09ec10cb43fad0ed8
Rafael Rossi <rossirafael@yahoo.com>
2018-03-29 18:27:24 +0200

    Orbits update for n-planets. Implemented show orbit.
    Still need to find a better way to plot without
    scaling manually, and implement zoom.

--- Orbits/src/orbits/Orbits.java
+++ Orbits/src/orbits/Orbits.java
@@ -1,7 +1,15 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Orbits
+ *
+ * Draws the orbits of planets around the sun.
+ *
+ * Calculations are done using classic Newton mechanics with planet data
+ * from NASA
+ * https://nssdc.gsfc.nasa.gov/planetary/factsheet/
+ *
+ * Calculations are done in real scale and scaled to fit canvas. Need to find a better way of
+ * doing that, also implement zoom.
+ *
  */
 package orbits;
 import javax.swing.*;
@@ -9,7 +17,7 @@ import java.awt.*;
 import java.awt.event.*;
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
-import java.util.Random;
+import static java.lang.Math.max;
 
 /**
  *
@@ -22,12 +30,9 @@ public class Orbits implements KeyListener //, MouseMotionListener, MouseListene
     Planet[] planets;
     
     boolean debug = false;
-    
-    double totalMass;
-    double[] cog = new double[2];
+    int timer = 10;
     
     boolean showOrbits = false;
-    
     
     public static void main(String[] args) {
         System.out.println("main()");
@@ -44,7 +49,7 @@ public class Orbits implements KeyListener //, MouseMotionListener, MouseListene
         //frame.addMouseMotionListener(this);
         //frame.addMouseListener(this);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1000, 1000);
         frame.setResizable(true);
         frame.setVisible(true);
         initPlanets();
@@ -56,19 +61,38 @@ public class Orbits implements KeyListener //, MouseMotionListener, MouseListene
         
         rescale();
         
-        planets = new Planet[2];
-        planets[0] = new Planet("Sun", 1988500.0e24, 695700.0e3,
+        // Planet datasheets from:
+        // https://nssdc.gsfc.nasa.gov/planetary/factsheet/
+        planets = new Planet[5];
+        planets[0] = new Planet("Sun", 1988500.0e24, 695700.0e3, 
+                                new double[] {0.0, 0.0}, 
                                 new double[] {0.0, 0.0},
-                                new double[] {0.0, 0.0}, 50);
+                                Color.orange);
         planets[1] = new Planet("Earth", 5.9723e24, 6378.137e3,
-                                 new double[] {0.0, 30.29e3}, 
-                                 new double[] {-147.09e9, 0.0}, 400);
+                                new double[] {0.0, 30.29e3}, 
+                                new double[] {-147.09e9, 0.0},
+                                Color.blue);
+        planets[2] = new Planet("Venus", 4.8675e24, 6051.8e3,
+                                new double[] {0.0, -34.79e3},
+                                new double[] {108.94e9, 0.0},
+                                Color.yellow);
+        planets[3] = new Planet("Mercury", 0.33011e24, 2439.7e3,
+                                new double[] {38.86e3, 0.0}, new double[] {0.0, 69.82e9},
+                                Color.red);
+        planets[4] = new Planet("Mars", 0.64171e24, 3396.2e3,
+                                new double[] {-21.97e3, 0.0},
+                                new double[] {0.0, -249.23e9}, 
+                                Color.green);
+        // can't really see the moon at this scale...
+        // need to implement zoon =)
+        //planets[5] = new Planet("Moon", 0.07346e24, 1738.1e3,
+        //                         new double[] {-0.970e3, 30.29e3}, new double[] {-147.09e9, 0.4055e9});
     }
     
     private void rescale() {
         int w = panel.getWidth();
         int h = panel.getHeight();
-        Planet.drawScale = 299258415000.0 / h;
+        Planet.drawScale = 2 * 300e9 / h;
         Planet.drawShiftX = 10 + w/2;
         Planet.drawShiftY = 10 + h/2;
     }
@@ -77,16 +101,18 @@ public class Orbits implements KeyListener //, MouseMotionListener, MouseListene
         System.out.println("start()");
         System.out.println(panel.getWidth() + " " + panel.getHeight());
         while (true) {
-                
+            
+            // First update the speed of all planets
             for(Planet p: planets)
-                for(Planet p1:planets)
-                    if (p != p1)
-                        p.updateOrbit(p1.mass, p1.position, 24*3600);
+                p.updateVelocity(planets);
+            // and then update their positions
+            for(Planet p: planets)
+                p.updateOrbit();
             
             panel.repaint();
             
             try {
-                Thread.sleep(80);
+                Thread.sleep(timer);
             } catch (InterruptedException ex) {
                 System.out.println("OW NO! WHAT NOW??");
             }
@@ -96,12 +122,20 @@ public class Orbits implements KeyListener //, MouseMotionListener, MouseListene
     // KeyPressed interface
     @Override
     public void keyPressed(KeyEvent ev) {
-        System.out.print("Key pressed...");
+        System.out.print("Key pressed: ");
         switch (ev.getKeyCode()) {
             case KeyEvent.VK_O:
                 // restart
                 System.out.println("O");
                 showOrbits = !showOrbits;
+                System.out.println("   showOrbit " + showOrbits);
+                panel.repaint();
+                break;
+            case KeyEvent.VK_E:
+                System.out.println("E");
+                System.out.println("   erase orbits ");
+                for (Planet p: planets)
+                    p.orbitPoints = 0;
                 panel.repaint();
                 break;
             default:
@@ -121,22 +155,25 @@ public class Orbits implements KeyListener //, MouseMotionListener, MouseListene
             
             gfx.fillRect(0, 0, w, h);
             
-            gfx.setColor(Color.orange);
-            
             for (Planet p: planets) {
                 if (debug) {
                     System.out.println("Drawing" + p.name);
                     System.out.println("   pos " + p.positionDraw[0] + " " + p.positionDraw[1]);
                     System.out.println("   rad " + p.radiusDraw);
                 }
-                gfx.fillOval(p.positionDraw[0], p.positionDraw[1],
-                             p.radiusDraw, p.radiusDraw);
+                gfx.setColor(p.color);
+                gfx.fillOval(p.positionDraw[0]-p.radiusDraw, p.positionDraw[1]-p.radiusDraw,
+                             2*p.radiusDraw, 2*p.radiusDraw);
             }
             
             if (showOrbits) {
-                gfx.setColor(Color.orange);
+                if (debug) System.out.println("Showing orbits");
+                gfx.setColor(Color.gray);
                 for (Planet p: planets) {
+                    if (debug) System.out.println("  " + p.name);
                     for (int i=0; i < p.orbitPoints-1; i++) {
+                        if (debug)
+                            System.out.println("   " + p.orbitDraw[i][0] + "   " + p.orbitDraw[i][1]);
                         gfx.drawLine(p.orbitDraw[i][0], p.orbitDraw[i][1],
                                      p.orbitDraw[i+1][0], p.orbitDraw[i+1][1]);
                     }
@@ -152,6 +189,8 @@ class Planet {
     static final double G = 6.674e-11;
     static double drawScale;
     static int drawShiftX, drawShiftY;
+    static double dt = 24*3600;
+    
     boolean debug = false;
     
     String name;
@@ -160,62 +199,95 @@ class Planet {
     double[] velocity = new double[2];
     double[] position = new double[2];
     int[] positionDraw = new int[2];
-    double[][] orbit = new double[5000][2];
-    int[][] orbitDraw = new int[5000][2];
-    int orbitPoints = 0;
-    double drawExageration = 1;
+    final int size = 5000;
+    double[][] orbit;
+    int[][] orbitDraw;
+    int orbitPoints;
+    Color color;
     
     double[] u = new double[2];
-    double distanceCoG;
+    double distance;
     
     public Planet(String nm, double m, double r,
-                  double[] v, double[] p, double f) {
+                  double[] v, double[] p, Color c) {
         System.out.println("Planet() " + nm);
         name = nm;
         mass = m;
         radius = r;
         position = p;
         velocity = v;
-        orbit[orbitPoints++] = position;
+        color = c;
+        
+        orbit = new double[size][2];
+        orbitDraw = new int[size][2];
+        orbitPoints = 0;
+        orbit[orbitPoints][0] = position[0];
+        orbit[orbitPoints][1] = position[1];
         positionDraw[0] = (int)(position[0]/drawScale) + drawShiftX;
         positionDraw[1] = (int)(position[1]/drawScale) + drawShiftY;
-        orbitDraw[orbitPoints-1] = positionDraw;
-        drawExageration = f;
-        radiusDraw = (int)(drawExageration*radius/drawScale);
+        orbitDraw[orbitPoints][0] = positionDraw[0];
+        orbitDraw[orbitPoints][1] = positionDraw[1];
+        orbitPoints++;
+        radiusDraw = max(8, 2*(int)(radius/drawScale));
         System.out.println("   Position " + position[0] + " "+ position[1]);
         System.out.println("   Position Canvas " + positionDraw[0] + " "+ positionDraw[1]);
         
     }
     
-    public void updateOrbit(double m, double[] cog, double dt) {
-                       
-        distanceCoG = sqrt(pow(position[0] - cog[0], 2) + 
-                           pow(position[1] - cog[1], 2));
+    public void updateVelocity(Planet[] planets) {
+        if (debug)
+            System.out.println("Updating velocity of " + name);
+        double a;
+        for (Planet p: planets) {
+            if (p != this) {
+                if (debug)
+                    System.out.println("   with " + p.name);
+                
+                distance = sqrt(pow(position[0] - p.position[0], 2) + 
+                                   pow(position[1] - p.position[1], 2));
         
-        u[0] = -(position[0] - cog[0]) / distanceCoG;
-        u[1] = -(position[1] - cog[1]) / distanceCoG;
+                // Unit vector pointing towards the other planet
+                u[0] = -(position[0] - p.position[0]) / distance;
+                u[1] = -(position[1] - p.position[1]) / distance;
         
-        double a = G * m / pow(distanceCoG, 2);
+                // Classic Newton mechanic:
+                // F = m1.a = G.m1.m2.a/r² ==> a = G.m2/r²
+                a = G * p.mass / pow(distance, 2);
         
-        velocity[0] += a*u[0]*dt;
-        velocity[1] += a*u[1]*dt;
+                velocity[0] += a*u[0]*dt;
+                velocity[1] += a*u[1]*dt;
+            }
+        }
+    }
+    public void updateOrbit() {
+        
         position[0] += velocity[0]*dt; 
         position[1] += velocity[1]*dt;
         
-        if (orbitPoints == 499)
-            orbitPoints = 0;
+        // Check if need to re-allocate orbit arrays
+        if (orbitPoints == orbit.length) {
+            if (debug) System.out.println("Increasing orbit array size...");
+            double[][] tmp = new double[orbit.length + size][2];
+            int[][] tmp1 = new int[orbitDraw.length + size][2];
+            System.arraycopy(orbit, 0, tmp, 0, orbit.length);
+            System.arraycopy(orbitDraw, 0, tmp1, 0, orbitDraw.length);
+            orbit = tmp;
+            orbitDraw = tmp1;
+        }
         
-        orbit[orbitPoints++] = position;
+        orbit[orbitPoints][0] = position[0];
+        orbit[orbitPoints][1] = position[1];
         
         positionDraw[0] = (int)(position[0]/drawScale) + drawShiftX;
         positionDraw[1] = (int)(position[1]/drawScale) + drawShiftY;
-        orbitDraw[orbitPoints-1] = positionDraw;
+        orbitDraw[orbitPoints][0] = positionDraw[0];
+        orbitDraw[orbitPoints][1] = positionDraw[1];
+        orbitPoints++;
         
         if (debug) {
             System.out.println("Updating orbit of " + name);
-            System.out.println("   Dist " + distanceCoG);
+            System.out.println("   Dist " + distance);
             System.out.println("   u " + u[0] + " " + u[1]);
-            System.out.println("   a " + a);
             System.out.println("   vel " + velocity[0] + " " + velocity[1]);
             System.out.println("   pos " + position[0] + " " + position[1]);
             System.out.println("   pos dwg " + positionDraw[0] + " " + positionDraw[1]);
